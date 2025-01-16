ab4ebeba9645435cf6f387370e5b6b8dcf9ae4e6
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-01-16 22:05:01 +0530

    added mongoDB atlas

--- server/app.js
+++ server/app.js
@@ -15,10 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    credentials:true
-}));
+app.use(
+    cors({
+      origin: ["http://localhost:5173", "https://code-craft001.vercel.app"],
+      credentials: true,
+    })
+  );
 app.use(cookieParser());
 app.use(morgan('dev'))
 
